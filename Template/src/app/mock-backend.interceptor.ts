dec91aeabbafe91cf79ff2fcf433f9b2178b6a2d
xelor <matthias.drescher@bytePassion.de>
2018-09-19 19:07:40 +0200

    added translations and mockbackend to access it

--- Template/src/app/mock-backend.interceptor.ts
+++ Template/src/app/mock-backend.interceptor.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse, HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Numbers } from './models/numbers';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class MockBackend implements HttpInterceptor {
 
+  private page1Alert1 = "One example to show in an alert";
+  private page1Alert2 = "Another example to show in an alert";
+  private page1Counter = 0;
+
+  constructor(private readonly httpClient: HttpClient) { }
+
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
     if (request.url.endsWith('/api/pageOne') && request.method === 'GET') {
-      return of(new HttpResponse({ status: 200, body: 'string data from the Backend!' }));
+
+      const content = (this.page1Counter++ % 2 === 0) ? this.page1Alert1 : this.page1Alert2;
+      return of(new HttpResponse({ status: 200, body: content }));
     }
 
     if (request.url.endsWith('/api/pageTwo') && request.method === 'GET') {
@@ -18,7 +27,6 @@ export class MockBackend implements HttpInterceptor {
         first: this.getRandomInt(200, 800),
         second: this.getRandomInt(2000, 8000)
       };
-
       return of(new HttpResponse({ status: 200, body: fancyInts }));
     }
 
@@ -28,8 +36,24 @@ export class MockBackend implements HttpInterceptor {
         first: this.getRandomFloat(0, 100),
         second: this.getRandomFloat(1100, 2000)
       };
-
       return of(new HttpResponse({ status: 200, body: fancyNumbers }));
+    }
+
+    if (request.url.endsWith('/api/translation/en') && request.method === 'GET') {
+
+      return this.httpClient.get("assets/translations/en.json")
+                 .pipe(map((res: any) => new HttpResponse({ status: 200, body: res })));      
+    }
+
+    if (request.url.endsWith('/api/translation/de') && request.method === 'GET') {
+
+      return this.httpClient.get("assets/translations/de.json")
+                 .pipe(map((res: any) => new HttpResponse({ status: 200, body: res })));      
+    }
+
+    if (request.url.endsWith('/api/translation/la') && request.method === 'GET') {
+
+      return of(new HttpResponse({ status: 200, body: JSON.parse("") }));
     }
 
     return next.handle(request);
